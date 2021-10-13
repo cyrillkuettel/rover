705c2e508db74468d94769e556c49670c80bc02c
Küttel Cyrill ckuttel <cyrill.kuettel@stud.hslu.ch>
2021-10-13 16:10:25 +0200

    added Websockets example

--- app/main.py
+++ app/main.py
@@ -15,7 +15,6 @@ app.mount(
     name="static",
 )
 
-
 current_file = Path(__file__)
 current_file_dir = current_file.parent  # /code/app
 project_root = current_file_dir.parent  # /code/
@@ -35,6 +34,45 @@ with open(INDEX_HTML_PATH, 'r') as f:
 
 """
 
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:80/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+@app.get("/test/")
+async def get():
+    return HTMLResponse(html)
+
 
 @app.get("/")
 async def root(request: Request):
@@ -43,4 +81,9 @@ async def root(request: Request):
     )
 
 
-
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
