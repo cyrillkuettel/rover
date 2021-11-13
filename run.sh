3444e4db3c9fe55a816248d34d44fff1feb160b1
Cyrill <cyrillkuettel@gmail.com>
2021-11-13 02:34:45 +0100

    wrote a nice convenient script to restart containers

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+docker stop $(docker ps -a -q)
+docker rm $(docker ps --filter status=exited -q)
 docker run -d --name mycontainer -p 80:80 -e MODULE_NAME="app.main" -e VARIABLE_NAME="app" -v /home/cyrill/rover/app:/code/app myimage /start-reload.sh
