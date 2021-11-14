769dedad213d6a96e12c3295fb8f2cd26057432c
Cyrill <cyrillkuettel@gmail.com>
2021-11-14 11:39:20 +0100

    improved websocket_endpoint to support commands and upload file functionality

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, WebSocket, Request, WebSocketDisconnect
+from fastapi import FastAPI, WebSocket, Request, WebSocketDisconnect, File, UploadFile
 from typing import Optional, List
 from pathlib import Path
 import logging
@@ -32,10 +32,12 @@ Incoming_Logs = []
 current_file = Path(__file__)
 current_file_dir = current_file.parent  # /code/app
 TEMPLATES = current_file_dir / "templates"
-FILES = current_file_dir / "files"
-APP = FILES / "pilot.apk"
+UPLOAD = current_file_dir / "upload"
+APP = UPLOAD / "pilot.apk"
+
 static = current_file_dir / "static"
 STATIC_IMG = static / "img"
+
 FAVICON = STATIC_IMG / "favicon.ico"
 templates = Jinja2Templates(directory=TEMPLATES)
 
@@ -93,21 +95,39 @@ async def del_cache(request: Request):
     return "<h2>Cleared Cache :)</h2>"
 
 
+@app.post("/apk/upload/")
+async def image(file: UploadFile = File(...)):
+    name = file.filename.replace(" ", "-")
+    file_name = UPLOAD / name
+    with open(file_name, 'wb+') as f:
+        f.write(file.file.read())
+        f.close()
+    logging.info(f"uploaded file. FILENAME = {file_name}")
+    APP = file_name  # can't access variable. ( TODO: Wrap in class )
+    return {"Uploaded File": file_name}
+
+
 @app.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: int):
     await manager.connect(websocket)
     try:
         while True:
-            #  Here we can create if statements for the type of socket connection and what kind of information it will
-            # convey.
-            stamp = get_timestamp()
             data = await websocket.receive_text()
-            Incoming_Logs.append(f"{stamp}: {data}")  # Just to store all Logs on the server side as well.
-            # This effectively reloads them from memory, the next time the page is fully reloaded.
-            # Thus, we have achieved a "primitive persistence functionality"
             logging.info("received Text:" + data)
-            await manager.send_personal_message(f"You wrote: {data}", websocket)  # this is not really necessary
-            await manager.broadcast(f"{stamp}: {data}")
+            if len(str(client_id)) <= 9:  # It's a Smartphone with The app
+                if "command=" in data:
+                    command = data[:]
+                    splitted = command.split("command=", 1)[1]
+                    logging.info(splitted)
+                    await manager.broadcast("splitted")
+                else:  # Normal Log
+                    stamp = get_timestamp()
+                    Incoming_Logs.append(f"{stamp}: {data}")  # Just to store all Logs on the server side as well.
+                    # This effectively reloads them from memory, the next time the page is fully reloaded.
+                    # Thus we have achieved a primitive kind of persistence
+            else:  # this client is a visitor of the Website.
+                message = f"You wrote: {data}", websocket
+                await manager.send_personal_message(message)  # this is not really necessary
+                await manager.broadcast(f"{stamp}: {data}")
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        # await manager.broadcast(f"Client #{client_id} left the chat")
