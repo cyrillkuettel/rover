559db6d15f4711349c0038961ae771a98a370db9
Cyrill <cyrillkuettel@gmail.com>
2021-11-14 11:06:59 +0100

    run, build, and rm container, pipe output into docker logs -f, all in one script

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 docker stop $(docker ps -a -q)
 docker rm $(docker ps --filter status=exited -q)
-docker run -d --name mycontainer -p 80:80 -e MODULE_NAME="app.main" -e VARIABLE_NAME="app" -v /home/cyrill/rover/app:/code/app myimage /start-reload.sh
+docker run -d --name mycontainer -p 80:80 -e MODULE_NAME="app.main" -e VARIABLE_NAME="app" -v /home/cyrill/rover/app:/code/app myimage /start-reload.sh | xargs -I{} docker logs -f {}
+
+
