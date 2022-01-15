26e7e4bf1a7e77efa2f07d59ca35b74b7ce90033
Cyrill <cyrillkuettel@gmail.com>
2022-01-15 21:00:15 +0100

    adding tabs + some css tweaks

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps --filter status=exited -q)
 docker build -t myimage .
-docker run -d --name mycontainer -p 80:80 -e MODULE_NAME="app.main" -e VARIABLE_NAME="app" -v /home/demo/rover/app:/code/app myimage /start-reload.sh | xargs -I{} docker logs -f {}
+docker run -d --name mycontainer -p 80:80 -e MODULE_NAME="app.main" -e VARIABLE_NAME="app" -v /home/cyrill/rover/app:/code/app myimage /start-reload.sh | xargs -I{} docker logs -f {}
 
 
