4e461c3e52929d1fbfd5bccc241ee72cd1d77357
Cyrill <cyrillkuettel@gmail.com>
2022-01-15 23:18:19 +0100

    removing cached files with "git rm -rf --cached ."  so that .gitignore applies

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker stop $(docker ps -a -q)
-docker rm $(docker ps --filter status=exited -q)
-docker build -t myimage .
-docker run -d --name mycontainer -p 80:80 -e MODULE_NAME="app.main" -e VARIABLE_NAME="app" -v /home/demo/rover/app:/code/app myimage /start-reload.sh | xargs -I{} docker logs -f {}
-
-
