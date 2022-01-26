3d0b9ab81fd357dbefd4f8e94a3ba5c3a781b670
Cyrill Küttel <36520284+cyrillkuettel@users.noreply.github.com>
2022-01-26 17:03:41 +0100

    Update all_commands_for_new_dropet.sh

--- all_commands_for_new_dropet.sh
+++ all_commands_for_new_dropet.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 #All commands for setting up a new Droplet 
 
+# Manually:
 adduser cyrill
 usermod -aG sudo cyrill
 
+
+# before re-logging with my username, have to: 
+sudo nano /etc/ssh/sshd_config
+PasswordAuthentication yes
+sudo service sshd restart
+
+# Now you can log in from the new user. We're almost done.
+mkdir ~/.ssh
+nano ~/.ssh/authorized_keys # Paste your ssh public key here
+sudo service sshd restart
+sudo nano /etc/ssh/sshd_config  # Now disable password authentification again:
+PasswordAuthentication no
+
+# Automatically
 sudo apt-get update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
