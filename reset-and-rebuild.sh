9bf3f2237525c876ba567a591918ca4f31f7913a
Cyrill <cyrillkuettel@gmail.com>
2022-04-13 22:39:19 +0200

    update .gitignore

--- reset-and-rebuild.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-echo "Let's run! Starting processs: compose down, build and up --detached."
-docker-compose down
-git fetch origin
-git reset --hard origin/main
-docker-compose build
-docker-compose up --build --detach
