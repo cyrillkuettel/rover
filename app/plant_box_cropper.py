26fdbd42bcd4cdd9ea6abc859517eb46d2e225e8
Cyrill <cyrillkuettel@gmail.com>
2022-07-03 15:09:40 +0200

    cropped image is saved in correct color format

--- app/plant_box_cropper.py
+++ app/plant_box_cropper.py
@@ -2,13 +2,15 @@ import torch
 from pandas import DataFrame
 from PIL.Image import Image
 import logging
-import numpy as np
+from pathlib import Path
 
+import numpy as np
+import cv2
 
 class PlantBoxCropper:
     """ This class can detect the bounding box of an image, cutting out the desired objects """
 
-    def __init__(self, link):
+    def __init__(self, link, image_save_path: Path):
         self.link = link
         self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
         self.model.conf = 0.25  # NMS confidence threshold
@@ -18,6 +20,7 @@ class PlantBoxCropper:
         self.model.classes = [58]  # potted plant  (COCO index for class)
         self.model.max_det = 1  # maximum number of detections per image, for now set it to 1 for simplicity
         self.model.amp = False  # Automatic Mixed Precision (AMP) inference
+        self.root: Path = image_save_path  # the path where we save the outputs
 
     def get_Number_of_plant_vase(self) -> int:
         number_of_detection_results = len(self.get_pandas_box_predictions())
@@ -36,6 +39,14 @@ class PlantBoxCropper:
         crops = results.crop(save=True)
         img_array = crops[0].get('im')  # crops is a list of dicts
         return img_array
+
+    def save_image(self, filename):
+        image = self.save_and_return_cropped_image()
+        imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        from PIL import Image
+        img = Image.fromarray(imageRGB)
+        test_image_cropped: Path = self.root / filename
+        img.save(test_image_cropped)
 
     def filter_plant_vase(self, df: DataFrame) -> DataFrame:
         """ method is not used, as we can just restrict search to potted plant in the __init__ config """
--- app/test_torch.py
+++ app/test_torch.py
@@ -3,14 +3,14 @@ import unittest
 import logging
 import numpy as np
 from pathlib import Path
-
 from PIL.Image import Image
-
 from app.plant_box_cropper import PlantBoxCropper
 from pandas import DataFrame
 
 
+
 def get_test_image_path():
+    """ Returns an Operating System agnostic path of the test image. """
     current_file = Path(__file__)
     current_file_dir = current_file.parent
     static = current_file_dir / "static"
@@ -18,12 +18,19 @@ def get_test_image_path():
     return test_img
 
 
-class MyTestCase(unittest.TestCase):
+class TestCaseBase(unittest.TestCase):
+    def assertIsFile(self, path):
+        """ helper function to assert the existence of a file or folder """
+        if not Path(path).resolve().is_file():
+            raise AssertionError("File does not exist: %s" % str(path))
+
+
+class MyTestCase(TestCaseBase):
 
     def test_get_number_of_plant_vase(self):
         Log = logging.getLogger("Test.torch")
         cropper = PlantBoxCropper(
-            "https://www.ikea.com/ch/en/images/products/clusia-potted-plant__0634293_pe697503_s5.jpg?f=s")
+            "https://www.ikea.com/ch/en/images/products/clusia-potted-plant__0634293_pe697503_s5.jpg?f=s", get_test_image_path())
         # Image with 1 potted plant
         box_pred: DataFrame = cropper.get_pandas_box_predictions()
         plant_vase_rows: DataFrame = cropper.filter_plant_vase(box_pred)
@@ -33,28 +40,39 @@ class MyTestCase(unittest.TestCase):
     def test_get_number_of_plant_vase2(self):
         link = "https://post.healthline.com/wp-content/uploads/2020/05/435791-Forget-You-Have-Plants-11-Types-That" \
                "-Will-Forgive-You_Thumnail-732x549.jpg "
-        cropper = PlantBoxCropper(link)  # 2 Plants
+        cropper = PlantBoxCropper(link, get_test_image_path())  # 2 Plants
         box_pred: DataFrame = cropper.get_pandas_box_predictions()
         plant_vase_count = len(cropper.filter_plant_vase(box_pred))
         self.assertGreaterEqual(plant_vase_count, 1)
 
     def test_get_cropped_image(self):
         cropper = PlantBoxCropper(
-            "https://www.ikea.com/ch/en/images/products/clusia-potted-plant__0634293_pe697503_s5.jpg?f=s")  # Image
+            "https://www.ikea.com/ch/en/images/products/clusia-potted-plant__0634293_pe697503_s5.jpg?f=s", get_test_image_path())  # Image
         # with 1 potted plant
         im = cropper.filter_plant_vase(cropper.get_pandas_box_predictions())
         self.assertIsNotNone(im)
 
     def test_load_local_image(self):
         """ it is a requirement to use local files """
-        link = get_test_image_path()
-        test_image = link / "potted_plant.jpg"
-        cropper = PlantBoxCropper(test_image)
+        root = get_test_image_path()
+        test_image = root / "potted_plant.jpg"
+        cropper = PlantBoxCropper(test_image, root)
         im = cropper.save_and_return_cropped_image()
         Log = logging.getLogger("Test.torch")
         Log.info(type(im))
         self.assertIsNotNone(im)
         self.assertIsInstance(im, np.ndarray)
+
+    def test_crop_image_and_save(self):
+        """ Tests if we can find the bounding box of test_img, get the result np.ndarray and save that to disk"""
+        root = get_test_image_path()
+        test_image = root / "potted_plant.jpg"
+        cropper = PlantBoxCropper(test_image, root)
+        output = "cropped_potted_plant.jpg"
+        cropper.save_image(output)
+
+        test_image_cropped = root / output
+        self.assertIsFile(test_image_cropped)
 
 
 if __name__ == '__main__':
