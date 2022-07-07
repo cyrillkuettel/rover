e2424e51e382f26d1bed40d81cbd2374b319387b
Cyrill <cyrillkuettel@gmail.com>
2022-07-07 02:43:50 +0200

    remove deletion of test.db in script, no sense

--- reset_and_rebuild.sh
+++ reset_and_rebuild.sh
@@ -4,6 +4,5 @@ echo "Let's run! Starting processs: compose down, build and up --detached."
 docker-compose down
 git fetch origin
 git reset --hard origin/main
-rm -f ~/rover/app/test.db # very important clear the database to have a clean start
 docker-compose build
 docker-compose up --build --detach
